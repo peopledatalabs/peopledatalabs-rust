e664d98bfe45508ce4ef17a70b35e23e5a0e8adb
Josh Finnie <josh.finnie@peopledatalabs.com>
2023-08-25 11:28:44 -0400

    Update main.rs

--- examples/ip/main.rs
+++ examples/ip/main.rs
@@ -1,12 +1,11 @@
-use peopledatalabs::{BaseParams, IPBaseParams, IPParams, PDL};
+use peopledatalabs::{IPBaseParams, IPParams, PDL};
 
 fn main() {
     let client = PDL::new();
-    let base_params = BaseParams::default();
     let mut ip_base_params = IPBaseParams::default();
     ip_base_params.ip = Some("72.212.42.169".to_string());
     let params = IPParams {
-        base_params,
+        base_params: None,
         ip_base_params,
     };
 
